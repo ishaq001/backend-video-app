19cd99ddcf5818d4b3ce127b06ae2ed171ad616d
Ishaq <orakk2211@gmail.com>
2024-05-30 03:58:57 +0500

    cloudinary added for files

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,21 @@
 import { Schema, model } from "mongoose";
 import bcrypt from "bcrypt";
 
+/**
+ * User schema for MongoDB.
+ * @typedef {Object} User
+ * @property {string} username - The username of the user.
+ * @property {string} email - The email of the user.
+ * @property {string} fullName - The full name of the user.
+ * @property {string} avatar - The URL of the user's avatar image.
+ * @property {string} coverImage - The URL of the user's cover image.
+ * @property {ObjectId} [watchHistory] - The ID of the video the user is watching.
+ * @property {string} password - The hashed password of the user.
+ * @property {string} [refreshToken] - The refresh token of the user.
+ * @property {Date} [createdAt] - The timestamp when the user was created.
+ * @property {Date} [updatedAt] - The timestamp when the user was last updated.
+ */
+
 const userSchema = new Schema(
 	{
 		username: {
@@ -48,16 +63,29 @@ const userSchema = new Schema(
 	}
 );
 
+/**
+ * Pre-save hook to hash the password before saving the user.
+ * @param {User} next - The next middleware function in the chain.
+ */
 userSchema.pre("save", async function (next) {
 	if (!this.isModified("password")) return next(); // if tha password is not modified, do nothing and next
 	this.password = await bcrypt.hash(this.password, 10);
 	next();
 });
 
+/**
+ * Method to check if the provided password is correct.
+ * @param {string} password - The password to be checked.
+ * @returns {Promise<boolean>} - A promise that resolves to true if the password is correct, false otherwise.
+ */
 userSchema.methods.isPasswordCorrect = async function (password) {
 	return await bcrypt.compare(password, this.password);
 };
 
+/**
+ * Method to generate an access token for the user.
+ * @returns {string} - The access token.
+ */
 userSchema.methods.generateAccessToken = function () {
 	return jwt.sign(
 		{ _id: this._id, emaill: this.email },
@@ -68,6 +96,10 @@ userSchema.methods.generateAccessToken = function () {
 	);
 };
 
+/**
+ * Method to generate a refresh token for the user.
+ * @returns {string} - The refresh token.
+ */
 userSchema.methods.generateRefreshToken = function () {
 	return jwt.sign({ _id: this._id }, process.env.REFRESH_TOKEN, {
 		expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,6 +1,20 @@
 import { Schema, model } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
+/**
+ * @typedef VideoSchema
+ * @description Schema for the Video collection in the database.
+ * @property {String} videoFile - Cloudinary URL for the video file.
+ * @property {String} thumbnail - Cloudinary URL for the video thumbnail.
+ * @property {String} title - Title of the video.
+ * @property {String} description - Description of the video.
+ * @property {Number} duration - Duration of the video.
+ * @property {ObjectId} owner - ID of the user who owns the video.
+ * @property {Number} [views=0] - Number of views for the video. Defaults to 0.
+ * @property {Boolean} [isPublished=true] - Whether the video is published. Defaults to true.
+ * @property {Boolean} timestamps - Whether to automatically add createdAt and updatedAt fields.
+ */
+
 const videoSchema = new Schema(
 	{
 		videoFile: {
@@ -40,6 +54,10 @@ const videoSchema = new Schema(
 	}
 );
 
+/**
+ * @function addAggregatePaginatePlugin
+ * @description Adds the mongoose-aggregate-paginate-v2 plugin to the Video schema.
+ */
 videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = model("Video", videoSchema);
